65be1ebbcb3d9f45d9a563c704aa3b2f3533f6a7
fotomain <foto888999@gmail.com>
2024-12-12 17:37:27 +0200

    111

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ const resolvers = {
 
       const keyData = args.game.id
       if((undefined===keyData) || (""===keyData))
-        game.id = uuid()
+        game.id = uuid4()
             // Math.floor(Math.random() * 10000).toString()
 
       db.games.push(game)
