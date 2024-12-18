b7c82035ac23bb7cd1ec1ae4d799fc65a8553648
fotomain <foto888999@gmail.com>
2024-12-18 10:24:56 +0200

    111

--- connectMongo.js
+++ connectMongo.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 mongoose.set("strictQuery", false);
 
-const localUri = "mongodb+srv://work_user2:password777999password777999@cluster0.algml.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const localUri =
+    "mongodb+srv://work_user2:password777999password777999@cluster0.algml.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 // const localUri = "mongodb+srv://vtest777999@gmail.com:password777999password777999@cluster0.algml.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const url = process.env.MONGODB_CONNECT_URI || localUri
--- index.js
+++ index.js
@@ -4,6 +4,17 @@
 import express from "express"
 import cors from 'cors'
 
+import { MongoClient } from "mongodb"
+
+const localUri =
+    "mongodb+srv://work_user2:password777999password777999@cluster0.algml.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const mongodbConnectUrl = process.env.MONGODB_CONNECT_URI || localUri
+
+console.log("=== mongodbConnectUrl ",mongodbConnectUrl)
+
+const client = new MongoClient(mongodbConnectUrl);
+
 import {v4 as uuid4} from "uuid";
 
 import pkg from 'apollo-server-express'
@@ -76,6 +87,8 @@ const resolvers = {
 
       db.games.push(game)
 
+
+
       return game
     },
     deleteGame(_, args) {
@@ -85,6 +98,7 @@ const resolvers = {
     },
     updateGame(_, args) {
       db.games = db.games.map((g) => {
+
         if (g.id === args.id) {
           return {...g, ...args.edits}
         }
@@ -145,7 +159,27 @@ app.get('/status', (_, res) => {
 });
 
 // ================
+// ================ MONGO CLIENT
 // ================
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: 'Back to the Future' };
+    const movie = await movies.findOne(query);
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+// ================
+// ================ SERVER
 // ================
 
 await server.start()
