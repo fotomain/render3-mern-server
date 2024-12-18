6201fb0bb097c33821aee22525523c51b510ad02
fotomain <foto888999@gmail.com>
2024-12-18 17:58:22 +0200

    111

--- index.js
+++ index.js
@@ -195,7 +195,7 @@ const resolvers = {
       if(mongodbMode) {
         const workEntity = dbGames.collection('games');
 
-        console.log("=== workResponse update game args ",args)
+        console.log("=== workResponse update game args",args)
 
         const workResponse = await workEntity.updateOne({_id:args.id},
             { $set: { "title" : args.edits.title } }
